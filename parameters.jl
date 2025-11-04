1e9b9004ad028178acbfaeb6a2a36a8d53f590cb
Jonás <35703108+24jonas@users.noreply.github.com>
2025-11-03 18:43:19 -0600

    plots created

--- parameters.jl
+++ parameters.jl
@@ -1,26 +1,26 @@
 # --- Simulation Parameters ---
 num_fermions = 3
-dimensions = 2
-bead_counts = [2, 4, 8, 12, 16]
+dimensions = 3
+bead_counts = [256]
 
 tau_start = 0.25
-tau_stop = 140.25
+tau_stop = 20.25
 tau_values = range(tau_start, tau_stop, length=200)
 
 # --- High-Precision Settings ---
 # Set precision for BigFloat (in bits). 50 decimal digits ≈ 167 bits.
-bigfloat_precision = 400
+bigfloat_precision = 512
 
 #Propagator choice: PA, TI, 4A is written as FA
 propagator_choice = Symbol("FA")
 
-mode = "ham" # thermo, ham
+mode = "exact" # thermo, ham
 
-w = BigFloat(1/2) #For no interaction, set w=1
+w = BigFloat(1/1) #For no interaction, set w=1
 balanced = false
 
-run_id = "normal"
+run_id = "$mode _N$num_fermions D$dimensions"
 
 #Preview plot
-toplim =25
+toplim =15
 bottomlim = 0
