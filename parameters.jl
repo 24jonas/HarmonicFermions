f23b29d03691fb925367da7e82729e63aa44d10b
Jonás <35703108+24jonas@users.noreply.github.com>
2025-12-11 19:52:38 -0600

    beeg run

--- parameters.jl
+++ parameters.jl
@@ -1,19 +1,19 @@
 # --- Simulation Parameters ---
-num_fermions = 100
+num_fermions = 384
 dimensions = 2
-bead_counts = [200]
-tau_start = 0.25
-tau_stop = 10.25
-tau_values = range(tau_start, tau_stop, length=200)
+bead_counts = [32, 200]
+tau_start = 5.25
+tau_stop = 15.25
+tau_values = range(tau_start, tau_stop, length=100)
 
 # --- High-Precision Settings ---
 # Set precision for BigFloat (in bits). 50 decimal digits ≈ 167 bits.
-bigfloat_precision = 20000
+bigfloat_precision = 74000
 
 #Propagator choice: PA, TI, 4A is written as FA
 propagator_choice = Symbol("PA")
 
-mode = "exact" # thermo, ham
+mode = "thermo" # thermo, ham
 
 w = BigFloat(1/2) #For no interaction, set w=1
 balanced = false
@@ -21,5 +21,5 @@ balanced = false
 run_id = "comp_$mode _N$num_fermions _D$dimensions _$propagator_choice"
 
 #Preview plot
-toplim = 1000
+toplim = 3500
 bottomlim = 1
